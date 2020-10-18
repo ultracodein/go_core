f715aa4845e3a93183d5c077075e82c809a2d378
Sergey Skuratov <ultracodein@gmail.com>
2020-10-19 01:23:50 +0300

    added input loop and comments

--- go_lesson_02/engine/cmd/engine/main.go
+++ go_lesson_02/engine/cmd/engine/main.go
@@ -11,25 +11,48 @@ func main() {
 	urls := []string{"https://habr.com/", "https://www.cnews.ru/"}
 	var depth int = 1
 
-	search := parseSearchPhrase()
-
 	fmt.Printf("Today we have %d sites to scan!\n", len(urls))
 	storage := make(map[string]string)
 	collectData(urls, depth, storage)
 
-	found := findRelatedPages(storage, search)
+	search := parseSearchPhrase()
 
-	fmt.Println("Pages found:")
+	for {
+		var input = search
 
-	if len(found) > 0 {
-		for _, link := range found {
-			fmt.Printf("[%s] %s\n", storage[link], link)
+		// если поисковая строка не была передана при запуске приложения
+		// или была указана пустой - запрашиваем ее у пользователя
+		if input == "" {
+			fmt.Print("Enter search phrase and press ENTER to search ")
+			fmt.Print("(or just press ENTER to exit): ")
+			fmt.Scanln(&input)
+
+			// если пользователь ничего не указал - выходим из приложения
+			if input == "" {
+				break
+			}
 		}
-	} else {
-		fmt.Println("Nothing.")
+
+		found := findRelatedPages(storage, input)
+
+		fmt.Println("Pages found:")
+		if len(found) > 0 {
+			for _, link := range found {
+				fmt.Printf("[%s] %s\n", storage[link], link)
+			}
+		} else {
+			fmt.Println("Nothing.")
+		}
+
+		// если поисковая строка была передана при запуске приложения
+		// то дальнейший интерактив не требуется - выходим из приложения
+		if search != "" {
+			break
+		}
 	}
 }
 
+// parseSearchPhrase возвращает поисковое значение, переданное при запуске приложения
 func parseSearchPhrase() string {
 	searchPtr := flag.String("search", "", "word/phrase to search in page title")
 	flag.Parse()
@@ -41,6 +64,7 @@ func parseSearchPhrase() string {
 	return *searchPtr
 }
 
+// collectData накапливает в словаре ссылки/заголовки страниц с загруженных сайтов
 func collectData(urls []string, depth int, storage map[string]string) {
 	for i, url := range urls {
 		fmt.Printf("Scanning URL #%d: %s\n", i+1, url)
@@ -57,6 +81,8 @@ func collectData(urls []string, depth int, storage map[string]string) {
 	}
 }
 
+// findRelatedPages возвращает список ссылок на страницы,
+// в заголовках которых содержится искомая фраза
 func findRelatedPages(storage map[string]string, search string) (found []string) {
 	found = []string{}
 	escapedSearch := "(?i)" + regexp.QuoteMeta(search)
