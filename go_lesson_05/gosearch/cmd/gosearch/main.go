2c682b81606adbb3e6ffcee77bda1c4c949087d6
Sergey Skuratov <ultracodein@gmail.com>
2020-11-01 20:01:09 +0300

    copied new project structure

---
+++ go_lesson_05/gosearch/cmd/gosearch/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+
+	"gosearch/pkg/crawler"
+	"gosearch/pkg/crawler/spider"
+	"gosearch/pkg/engine"
+	"gosearch/pkg/index"
+	"gosearch/pkg/index/hash"
+	"gosearch/pkg/storage"
+	"gosearch/pkg/storage/memstore"
+)
+
+// Сервер Интернет-поисковика GoSearch.
+type gosearch struct {
+	engine  *engine.Service
+	scanner crawler.Interface
+	index   index.Interface
+	storage storage.Interface
+
+	sites []string
+	depth int
+}
+
+func main() {
+	server := new()
+	server.init()
+	server.searchLoop()
+}
+
+// new создаёт объект и службы сервера и возвращает указатель на него.
+func new() *gosearch {
+	gs := gosearch{}
+	gs.scanner = spider.New()
+	gs.index = hash.New()
+	gs.storage = memstore.New()
+	gs.engine = engine.New(gs.index, gs.storage)
+	gs.sites = []string{"https://habr.com", "https://cnews.ru/"}
+	gs.depth = 1
+	return &gs
+}
+
+// init производит сканирование сайтов и индексирование данных.
+func (gs *gosearch) init() {
+	log.Println("Сканирование сайтов.")
+	id := 0
+	for _, url := range gs.sites {
+		log.Println("Сайт:", url)
+		data, err := gs.scanner.Scan(url, gs.depth)
+		if err != nil {
+			continue
+		}
+		for i := range data {
+			data[i].ID = id
+			id++
+		}
+		log.Println("Индексирование документов.")
+		gs.index.Add(data)
+		log.Println("Сохранение документов.")
+		err = gs.storage.StoreDocs(data)
+		if err != nil {
+			log.Println("ошибка при добавлении документов в хранилище:", err)
+			continue
+		}
+	}
+}
+
+// run выполняет поиск документов
+func (gs *gosearch) searchLoop() {
+	// получаем значение поисковой строки из аргумента
+	// и проставляем флаг, если строка была получена именно таким способом
+	var fromCmd = false
+	word := parseSearchWord()
+	if word != "" {
+		fromCmd = true
+	}
+
+	for {
+		// если поисковая строка не была получена из аргумента
+		// или была указана пустой - запрашиваем ее у пользователя
+		if word == "" {
+			fmt.Print("Enter word and press ENTER: ")
+			fmt.Scanln(&word)
+			// если пользователь ничего не указал - выходим из приложения
+			if word == "" {
+				break
+			}
+		}
+
+		docs := gs.engine.Search(word)
+		text := genOutput(docs)
+		fmt.Print(text)
+
+		// если поисковая строка была получена из аргумента,
+		// то дальнейший интерактив не требуется - выходим из приложения
+		if fromCmd {
+			break
+		}
+
+		// сбрасываем значение поисковой строки
+		// для выдачи запроса на ее ввод в следующей итерации
+		word = ""
+	}
+}
+
+func genOutput(docs []crawler.Document) string {
+	if docs == nil {
+		return "Nothing.\n"
+	}
+
+	var text string = ""
+	for _, doc := range docs {
+		text += "[" + strconv.Itoa(doc.ID) + "] " + doc.Title + "\n"
+	}
+	return text
+}
+
+// parseSearchWord возвращает слово для поиска, переданное в виде флага при запуске приложения
+func parseSearchWord() string {
+	searchPtr := flag.String("search", "", "word to search in page title")
+	flag.Parse()
+	return *searchPtr
+}
---
+++ go_lesson_05/gosearch/pkg/index/hash/hash.go
@@ -0,0 +1,68 @@
+package hash
+
+import (
+	"gosearch/pkg/crawler"
+	"regexp"
+	"strings"
+)
+
+// Index - индекс на основе хэш-таблицы.
+type Index struct {
+	data map[string][]int
+}
+
+// New - конструктор.
+func New() *Index {
+	var ind Index
+	ind.data = make(map[string][]int)
+	return &ind
+}
+
+// Add добавляет данные из переданных документов в индекс.
+//
+// Сначала происходит выделение лексем как ключей словаря из данных документа.
+// Потом проверяется наличие номера документа в значении словаря для лексемы.
+// Если номер документа не найден, то он добавляется в значение словаря.
+func (index *Index) Add(docs []crawler.Document) {
+	for _, doc := range docs {
+		foundTokens := tokens(doc.Title)
+		if foundTokens == nil {
+			return
+		}
+
+		for _, token := range foundTokens {
+			if !exists(index.data[token], doc.ID) {
+				index.data[token] = append(index.data[token], doc.ID)
+			}
+		}
+	}
+}
+
+// Search возвращает номера документов, где встречается данная лексема.
+func (index *Index) Search(token string) []int {
+	return index.data[strings.ToLower(token)]
+}
+
+// Разделение строки на лексемы.
+func tokens(s string) []string {
+	wordPattern := regexp.MustCompile("[\\p{L}\\d]+")
+	words := wordPattern.FindAllString(s, -1)
+	if words == nil {
+		return nil
+	}
+
+	for i := range words {
+		words[i] = strings.ToLower(words[i])
+	}
+	return words
+}
+
+// Проверка наличия элемента в массиве.
+func exists(ids []int, item int) bool {
+	for _, id := range ids {
+		if id == item {
+			return true
+		}
+	}
+	return false
+}
