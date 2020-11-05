926ceeccdbdd4d8819ea526e19715268c35a2e55
Sergey Skuratov <ultracodein@gmail.com>
2020-11-06 02:48:04 +0300

    add scheduler

---
+++ go_lesson_07/gosearch/cmd/gosearch/main.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"gosearch/pkg/cache"
+	"gosearch/pkg/cache/bstcache"
+	"gosearch/pkg/crawler"
+	"gosearch/pkg/crawler/spider"
+	"gosearch/pkg/engine"
+	"gosearch/pkg/index"
+	"gosearch/pkg/index/hash"
+	"gosearch/pkg/scheduler"
+	"gosearch/pkg/storage"
+	"gosearch/pkg/storage/bststore"
+)
+
+// Сервер Интернет-поисковика GoSearch.
+type gosearch struct {
+	engine    *engine.Service
+	scanner   crawler.Interface
+	index     index.Interface
+	storage   storage.Interface
+	cache     cache.Interface
+	scheduler *scheduler.Service
+
+	cacheFiles    map[string]string
+	schedulerFile string
+	sites         []string
+	depth         int
+}
+
+func main() {
+	server := new()
+	server.loadCache()
+	go func() {
+		server.init()
+		server.saveCache()
+	}()
+	server.searchLoop()
+}
+
+// new создаёт объект и службы сервера и возвращает указатель на него.
+func new() *gosearch {
+	gs := gosearch{}
+	gs.scanner = spider.New()
+	gs.index = hash.New()
+	gs.storage = bststore.New()
+	gs.cacheFiles = map[string]string{
+		"index":   "index.bin",
+		"storage": "storage.bin",
+	}
+	gs.cache = bstcache.New(gs.cacheFiles)
+	gs.schedulerFile = "scheduler.bin"
+	gs.scheduler = gs.initScheduler()
+	gs.depth = 2
+	return &gs
+}
+
+func (gs *gosearch) initScheduler() *scheduler.Service {
+	s, err := scheduler.LoadFrom(gs.schedulerFile)
+	if err != nil {
+		sites := []string{"https://www.ixbt.com/", "https://habr.com/"}
+		expdays := 3
+		log.Println("Планировщик не найден (инициализирован начальными значениями).")
+		return scheduler.New(sites, expdays)
+	}
+	log.Println("Планировщик найден и загружен.")
+	return s
+}
+
+// loadCache загружает индекс и хранилище из кэша (если он есть)
+func (gs *gosearch) loadCache() {
+	_, err := os.Stat(gs.cacheFiles["index"])
+	if err != nil {
+		log.Println("Кэш не найден.")
+		return
+	}
+	_, err = os.Stat(gs.cacheFiles["storage"])
+	if err != nil {
+		log.Println("Кэш не найден.")
+		return
+	}
+
+	log.Println("Загрузка данных из кэша.")
+	ind, str, err := gs.cache.Load()
+	if err != nil {
+		log.Println(err)
+	}
+
+	gs.index = *ind
+	gs.storage = *str
+	gs.engine = engine.New(*ind, *str)
+}
+
+// saveCache сохраняет индекс и хранилище в кэш
+func (gs *gosearch) saveCache() {
+	log.Println("Сохранение данных в кэш.")
+	err := gs.cache.Create(&gs.index, &gs.storage)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+// init производит сканирование сайтов и индексирование данных.
+func (gs *gosearch) init() {
+	sites := gs.scheduler.ExpiredSites()
+	if len(sites) > 0 {
+		log.Println("Сканирование сайтов.")
+	}
+	id := 0
+	for _, url := range sites {
+		log.Println("Сайт:", url)
+		data, err := gs.scanner.Scan(url, gs.depth)
+		if err != nil {
+			continue
+		}
+		for i := range data {
+			data[i].ID = id
+			id++
+		}
+		log.Println("Индексирование документов.")
+		gs.index.Add(data)
+		log.Println("Сохранение документов.")
+		err = gs.storage.StoreDocs(data)
+		if err != nil {
+			log.Println("ошибка при добавлении документов в хранилище:", err)
+			continue
+		}
+		gs.scheduler.UpdateHistory(url)
+	}
+	if len(sites) > 0 {
+		gs.scheduler.SaveTo(gs.schedulerFile)
+	}
+	gs.engine = engine.New(gs.index, gs.storage)
+}
+
+// run выполняет поиск документов
+func (gs *gosearch) searchLoop() {
+	// получаем значение поисковой строки из аргумента
+	// и проставляем флаг, если строка была получена именно таким способом
+	var fromCmd = false
+	word := parseSearchWord()
+	if word != "" {
+		fromCmd = true
+	}
+
+	for {
+		if gs.engine == nil {
+			time.Sleep(10 * time.Second)
+			continue
+		}
+
+		// если поисковая строка не была получена из аргумента
+		// или была указана пустой - запрашиваем ее у пользователя
+		if word == "" {
+			fmt.Print("Enter word and press ENTER: ")
+			fmt.Scanln(&word)
+			// если пользователь ничего не указал - выходим из приложения
+			if word == "" {
+				break
+			}
+		}
+
+		docs := gs.engine.Search(word)
+		text := genOutput(docs)
+		fmt.Print(text)
+
+		// если поисковая строка была получена из аргумента,
+		// то дальнейший интерактив не требуется - выходим из приложения
+		if fromCmd {
+			break
+		}
+
+		// сбрасываем значение поисковой строки
+		// для выдачи запроса на ее ввод в следующей итерации
+		word = ""
+	}
+}
+
+func genOutput(docs []crawler.Document) string {
+	if docs == nil {
+		return "Nothing.\n"
+	}
+
+	var text string = ""
+	for _, doc := range docs {
+		text += "[" + strconv.Itoa(doc.ID) + "] " + doc.Title + "\n"
+	}
+	return text
+}
+
+// parseSearchWord возвращает слово для поиска, переданное в виде флага при запуске приложения
+func parseSearchWord() string {
+	searchPtr := flag.String("search", "", "word to search in page title")
+	flag.Parse()
+	return *searchPtr
+}
---
+++ go_lesson_07/gosearch/pkg/scheduler/scheduler.go
@@ -0,0 +1,74 @@
+package scheduler
+
+import (
+	"encoding/gob"
+	"os"
+	"time"
+)
+
+// Service реализует простой планировщик сканирований
+type Service struct {
+	History map[string]time.Time
+	ExpDays int
+}
+
+// New - конструктор.
+func New(sites []string, expdays int) *Service {
+	history := make(map[string]time.Time)
+	for _, site := range sites {
+		history[site] = time.Unix(0, 0)
+	}
+	s := Service{
+		History: history,
+		ExpDays: expdays,
+	}
+	return &s
+}
+
+// UpdateHistory обновляет даты сканирования сайтов в истории
+func (s *Service) UpdateHistory(site string) {
+	s.History[site] = time.Now().Truncate(time.Hour * 24)
+}
+
+// ExpiredSites возвращает устаревшие сайты
+func (s *Service) ExpiredSites() []string {
+	expired := make([]string, 0)
+	for site, scanDate := range s.History {
+		days := int(time.Now().Sub(scanDate).Hours() / 24)
+		if days >= s.ExpDays {
+			expired = append(expired, site)
+		}
+	}
+	return expired
+}
+
+// SaveTo сохраняет планировщик в файл
+func (s *Service) SaveTo(fileName string) error {
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	enc := gob.NewEncoder(file)
+	err = enc.Encode(s)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// LoadFrom создает планировщик из файла
+func LoadFrom(fileName string) (*Service, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var s Service
+	dec := gob.NewDecoder(file)
+	err = dec.Decode(&s)
+	if err != nil {
+		return nil, err
+	}
+	return &s, nil
+}
