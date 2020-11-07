4eb37db55abed77059d22aa08479c22b89e27105
Sergey Skuratov <ultracodein@gmail.com>
2020-11-07 17:45:07 +0300

    copied from previous lessons

---
+++ go_lesson_08/gosearch/pkg/cache/bstcache/bstcache.go
@@ -0,0 +1,88 @@
+package bstcache
+
+import (
+	"encoding/gob"
+	"os"
+
+	"gosearch/pkg/crawler"
+	"gosearch/pkg/index"
+	"gosearch/pkg/index/hash"
+	"gosearch/pkg/storage"
+	"gosearch/pkg/storage/bststore"
+)
+
+// Data - структура, реализующая контракт кэширующего модуля
+type Data struct {
+	indexFile, storageFile string
+}
+
+// New - конструктор.
+func New(cacheFiles map[string]string) *Data {
+	c := Data{
+		indexFile:   cacheFiles["index"],
+		storageFile: cacheFiles["storage"],
+	}
+	return &c
+}
+
+// Create сохраняет индекс и хранилище в файлы
+func (c *Data) Create(index *index.Interface, storage *storage.Interface) error {
+	indexFile, err := os.Create(c.indexFile)
+	if err != nil {
+		return err
+	}
+	defer indexFile.Close()
+	gob.Register(&hash.Index{})
+	gob.Register(&crawler.Document{})
+	enc := gob.NewEncoder(indexFile)
+	err = enc.Encode(index)
+	if err != nil {
+		return err
+	}
+
+	storageFile, err := os.Create(c.storageFile)
+	if err != nil {
+		return err
+	}
+	defer storageFile.Close()
+	gob.Register(&bststore.DB{})
+	enc = gob.NewEncoder(storageFile)
+	err = enc.Encode(storage)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Load загружает индекс и хранилище из файлов
+func (c *Data) Load() (*index.Interface, *storage.Interface, error) {
+	indexFile, err := os.Open(c.indexFile)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer indexFile.Close()
+	var indexData index.Interface
+	gob.Register(&hash.Index{})
+	gob.Register(&crawler.Document{})
+	dec := gob.NewDecoder(indexFile)
+	err = dec.Decode(&indexData)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	storageFile, err := os.Open(c.storageFile)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer storageFile.Close()
+	var storageData storage.Interface
+	gob.Register(&bststore.DB{})
+	dec = gob.NewDecoder(storageFile)
+	err = dec.Decode(&storageData)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &indexData, &storageData, nil
+}
---
+++ go_lesson_08/gosearch/pkg/index/hash/hash.go
@@ -0,0 +1,72 @@
+package hash
+
+import (
+	"gosearch/pkg/crawler"
+	"regexp"
+	"strings"
+)
+
+// Index - индекс на основе хэш-таблицы.
+type Index struct {
+	Data map[string][]int
+}
+
+// New - конструктор.
+func New() *Index {
+	var ind Index
+	ind.Data = make(map[string][]int)
+	return &ind
+}
+
+// Add добавляет данные из переданных документов в индекс.
+//
+// Сначала происходит выделение лексем как ключей словаря из данных документа.
+// Потом проверяется наличие номера документа в значении словаря для лексемы.
+// Если номер документа не найден, то он добавляется в значение словаря.
+func (index *Index) Add(docs []crawler.Document) {
+	if docs == nil {
+		return
+	}
+
+	for _, doc := range docs {
+		foundTokens := tokens(doc.Title)
+		if foundTokens == nil {
+			return
+		}
+
+		for _, token := range foundTokens {
+			if !exists(index.Data[token], doc.ID) {
+				index.Data[token] = append(index.Data[token], doc.ID)
+			}
+		}
+	}
+}
+
+// Search возвращает номера документов, где встречается данная лексема.
+func (index *Index) Search(token string) []int {
+	return index.Data[strings.ToLower(token)]
+}
+
+// Разделение строки на лексемы.
+func tokens(s string) []string {
+	wordPattern := regexp.MustCompile("[\\p{L}\\d]+")
+	words := wordPattern.FindAllString(s, -1)
+	if words == nil {
+		return nil
+	}
+
+	for i := range words {
+		words[i] = strings.ToLower(words[i])
+	}
+	return words
+}
+
+// Проверка наличия элемента в массиве.
+func exists(ids []int, item int) bool {
+	for _, id := range ids {
+		if id == item {
+			return true
+		}
+	}
+	return false
+}
