640c19a8e2997561850f2d678513465b99ff3c5e
Sergey Skuratov <ultracodein@gmail.com>
2020-11-15 01:43:28 +0300

    add bench tests and comparison result

--- go_lesson_09/gosearch/pkg/storage/btstore/btstore_test.go
+++ go_lesson_09/gosearch/pkg/storage/btstore/btstore_test.go
@@ -31,3 +31,30 @@ func TestDB_Docs(t *testing.T) {
 		t.Fatalf("got %v, want %v", got, want)
 	}
 }
+
+func BenchmarkDB_StoreDocs(b *testing.B) {
+	var docs = []crawler.Document{{ID: 1}, {ID: 2}}
+	db := New()
+
+	for n := 0; n < b.N; n++ {
+		db.StoreDocs(docs)
+		for i := range docs {
+			docs[i].ID++
+		}
+	}
+}
+
+var result []crawler.Document
+
+func BenchmarkDB_Docs(b *testing.B) {
+	var docs = []crawler.Document{{ID: 1}, {ID: 2}, {ID: 3}}
+	db := New()
+	db.StoreDocs(docs)
+	var found []crawler.Document
+
+	for n := 0; n < b.N; n++ {
+		found = db.Docs([]int{1, 3})
+	}
+
+	result = found
+}
